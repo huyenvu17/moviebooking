23d1c2c5577824d5e4b294b0341538a24634241e
Huyen Vu <huyenvungoc17@gmail.com>
2020-08-17 21:05:39 +0700

    create movie booking app

---
+++ movie-booking/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="display-4">đặt vé xem phim cyberlearn.vn</div>
+    </div>
+  );
+}
+
+export default App;
